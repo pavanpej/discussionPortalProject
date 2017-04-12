52fde2107bf8768a6748a0750fba1b93f70f4672
Prajwal Prashanth <prajapple@gmail.com>
2017-04-12 20:43:10 +0530

    added time stamp

--- entermsg.php
+++ entermsg.php
@@ -1,5 +1,7 @@
 <?php
 $name =$_POST['usrname'];
 $desg =$_POST['designation'];
-echo "login details: ".$name." ".$desg;
+date_default_timezone_set('Asia/Kolkata');
+$dnt= date("d/m/Y__h:i A");
+echo "login details: ".$name." ".$desg." ".$dnt;
 ?>
